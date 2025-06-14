57df858ed142275ec4dc4b034d7252017354b55a
Vito Sansevero <vito.sansevero@gmail.com>
2025-06-14 10:57:34 -0700

    feat: Add startup message and WEB_DIRECTORY variable

--- __init__.py
+++ __init__.py
@@ -5,4 +5,10 @@ All nodes are grouped under the "ComfyAssets" category
 
 from .kikotools import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+# Tell ComfyUI where to find our JavaScript extensions
+WEB_DIRECTORY = "./web"
+
+# Print startup message
+print("\033[94m[ComfyUI-KikoTools] Loaded with swap button support!\033[0m")
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
