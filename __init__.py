5031793b04f19b37a37b6d904dfa7361c8d428a9
Vito Sansevero <vito.sansevero@gmail.com>
2025-06-14 07:59:23 -0700

    feat(init): Add initial __init__.py for KikoTools module

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+ComfyUI-KikoTools: Modular collection of custom ComfyUI nodes
+All nodes are grouped under the "ComfyAssets" category
+"""
+
+from .kikotools import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
