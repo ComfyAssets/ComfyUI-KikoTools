b04077ec0073897e3a9c940c7dd1a851c425ad4a
Vito Sansevero <vito.sansevero@gmail.com>
2025-06-14 10:57:47 -0700

    feat(init): add WidthHeightSelector node

--- kikotools/__init__.py
+++ kikotools/__init__.py
@@ -4,14 +4,17 @@ Handles automatic discovery and registration of all ComfyAssets tools
 """
 
 from .tools.resolution_calculator import ResolutionCalculatorNode
+from .tools.width_height_selector import WidthHeightSelectorNode
 
 # ComfyUI node registration mappings
 NODE_CLASS_MAPPINGS = {
     "ResolutionCalculator": ResolutionCalculatorNode,
+    "WidthHeightSelector": WidthHeightSelectorNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ResolutionCalculator": "Resolution Calculator",
+    "WidthHeightSelector": "Width Height Selector",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
